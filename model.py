4c9e24fb90eec524f0500fec9403bfb3ca0a49d6
VimGnuLinuxGit <VimGnuLinuxGit@gmail.com>
2019-08-26 18:01:53 +0430

    10[model]
    Refactoring code and changes in algorithms
    and tryExcept method has been added

--- model.py
+++ model.py
@@ -11,60 +11,56 @@
 class Model(object):
 
     # Functions ----------------------------------------------------------------------------------------#
-    def press(self, char):
-        if self.entry.get()[:] == 'Error':    # Do nothing
-            return
-        self.entry.configure(stat='normal')
-        self.entry.configure(foreground='green')
-        self.entry.insert('end', char)
-
     def clearEntry(self):
         self.entry.configure(stat='normal')
         self.entry.configure(foreground='green')
         self.entry.delete('0', 'end')
 
-    def delEndChar(self):       
-        if self.entry.get()[:] == 'Error':    # Do nothing
-            return
-        self.entry.configure(stat='normal')
+    def delEndChar(self):
         text = self.entry.get()[:-1]
         self.entry.delete('0', 'end')
         self.entry.insert('0', text)
-    
+
+    def press(self, char):
+        if dict(self.entry)['state'] == 'normal':
+            self.entry.insert('end', char)
+
     def power(self):
-        resultEqual = self.equal()
-        if resultEqual == None:    # Do nothing
-            return
-        self.entry.configure(stat='normal')
-        self.entry.delete('0', 'end')
-        self.entry.insert('0', pow(resultEqual, 2))
-    
+        resultPower = self.tryExcept()
+        if resultPower != None:
+            self.entry.delete('0', 'end')
+            self.entry.insert('0', pow(resultPower, 2))
+
     def equal(self):
-        getEntry = self.entry.get()
-        try:
-            if len(getEntry) == 0:    # If entry as empty: do nothing 
-                return 
-            for item in getEntry:
-                if item in (',', ';', '^', '&', '|', '=', '!', '>', '<'):
-                    getEntry = 'Error input character'    # If characters input the are these: eval Error
-                    break
-                elif item == '÷':
-                    getEntry = getEntry.replace('÷', '/')
-                elif item == 'x':
-                    getEntry = getEntry.replace('x', '*')
-                elif item == '%':
-                    getEntry = getEntry.replace('%', '/100')
-            resultEqual = eval(getEntry)
-            self.entry.configure(stat='normal')
+        resultEqual = self.tryExcept()
+        if resultEqual != None:
             self.entry.delete('0', 'end')
             self.entry.insert('0', resultEqual)
-            return resultEqual
-        except:
+
+    def tryExcept(self):
+        getEntry = self.entry.get()
+        if len(getEntry) == 0:    # If entry as empty: do nothing 
+            return None 
+        for item in getEntry:
+            if item in (',', ';', '^', '&', '|', '=', '!', '>', '<'):
+                getEntry = 'Error'    # If characters input the are these: eval Error and SyntaxError
+                break
+            elif item == '÷':
+                getEntry = getEntry.replace('÷', '/')
+            elif item == 'x':
+                getEntry = getEntry.replace('x', '*')
+            elif item == '%':
+                getEntry = getEntry.replace('%', '/100')
+        try:
+            resultTryExcept = eval(getEntry)
+            return resultTryExcept
+        except (SyntaxError, NameError):    # If empty value is Error: NameError
             self.entry.delete('0', 'end')
             self.entry.configure(foreground='red')
             self.entry.insert('0', 'Error')
             self.entry.configure(stat='readonly')
-            return
+            return None
+
 
 
 if __name__ == "__main__":
